f7d7c4f6de9ec4ddb77506a218c264fab2eccd09
Aman Jain <ajinhindauncity@gmail.com>
2024-02-19 22:54:05 +0530

    redux

--- 004_Redux/01_Redux-Fundamental/script.js
+++ 004_Redux/01_Redux-Fundamental/script.js
@@ -1,16 +1,66 @@
-let reduxState = {
-    count: 0,
+import {createStore} from 'redux'
+
+const initialState = {
+    post: 0,
     age: 21,
     name: 'Aman Jain',
 }
 
+const INCREMENT = "post/increment"
+const DECREMENT = "post/decrement"
+const INCREASE_BY = "post/increaseBy"
+const DECREASE_BY = "post/decreaseBy"
+
 // reducer
-function reducer(state){
-    return {...state, count: state.count + 1}
+function reducer(state = initialState, action){
+    // if(action.type === INCREMENT){
+    //     return {...state, post: state.post + 1}
+    // }else if(action.type === DECREMENT){
+    //     return {...state, post: state.post - 1}
+    // }else if(action.type === INCREASE_BY){
+    //     return {...state, post: state.post + action.payload}
+    // }else if(action.type === DECREASE_BY){
+    //     return {...state, post: state.post - action.payload}
+    // }
+    // return state
+    switch(action.type){
+        case INCREMENT:
+            return {...state, post: state.post + 1}
+        case DECREMENT:
+            return {...state, post: state.post - 1}
+        case INCREASE_BY:
+            return {...state, post: state.post + action.payload}
+        case DECREASE_BY:
+            return {...state, post: state.post - action.payload}
+        default:
+            return state
+    }
 }
 
 // What redux will do ??
-reduxState = reducer(reduxState)
+
+const store = createStore(reducer)
+console.log(store)
+store.subscribe(() => {
+    console.log(store.getState())
+})
+store.dispatch({type: DECREMENT})
+store.dispatch({type: INCREMENT})
+store.dispatch({type: INCREASE_BY, payload: 10})
+store.dispatch({type: DECREASE_BY, payload: 7})
+
+// console.log(store.getState())
+// store.dispatch({type: "post/decrement"})
+// console.log(store.getState())
+
+// reduxState = reducer(reduxState, {type: "post/increment"})
+// console.log(reduxState)
+// reduxState = reducer(reduxState, {type: "post/increment"})
+// console.log(reduxState)
+// reduxState = reducer(reduxState, {type: "post/decrement"})
+// console.log(reduxState)
+// reduxState = reducer(reduxState, {type: "post/incrementBy", payload: 10})
+// console.log(reduxState)
 
 // let prevState = state
 
