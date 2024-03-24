810038c2927e189733504a10d6191946b930d383
Aman Jain <ajinhindauncity@gmail.com>
2024-03-24 16:27:18 +0530

    dynamic routing

--- 005_CodeSync-Project/frontend/src/main.jsx
+++ 005_CodeSync-Project/frontend/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import EditorPage from './pages/EditorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/editor/:roomId',
+        element: <EditorPage/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
