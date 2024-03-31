d52ec6df92ea35497cc439c9e34e3cebf813ef2d
Aman Jain <ajinhindauncity@gmail.com>
2024-03-31 22:19:01 +0530

    add a middleware and a function to handle errors

--- 007_MERN-BLOG/api/controllers/auth.controller.js
+++ 007_MERN-BLOG/api/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async(req, res) => {    
+export const signup = async(req, res, next) => {    
     const {username, email, password} = req.body
     if(!username || !email || !password || username === "" || email === "" || password === ""){
-        res.status(400).json({message: "All fields are required"})
+        // res.status(400).json({message: "All fields are required"})
+        next(errorHandler(400, 'All fields are required'))
     }
     const hashedPassword = bcryptjs.hashSync(password, 10)
     try {
@@ -15,6 +17,7 @@ export const signup = async(req, res) => {
         })
         res.status(201).json({user})
     } catch (error) {
-        res.status(400).json({message: error.message})
+        // res.status(400).json({message: error.message})
+        next(error)
     }
 }
