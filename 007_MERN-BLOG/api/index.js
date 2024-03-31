14d8dad14be50cf3cd462aed3730e16e31c9d140
Aman Jain <ajinhindauncity@gmail.com>
2024-03-31 21:02:16 +0530

    create a test api route

--- 007_MERN-BLOG/api/index.js
+++ 007_MERN-BLOG/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoute from './routes/user.route.js'
 
 dotenv.config()
 
@@ -16,3 +17,9 @@ const app = express()
 app.listen(3000, ()=>{
     console.log(`server is runnig on port 3000`)
 })
+
+// app.get('/', (req, res) => {
+//     res.json({message: "Api is working"})
+// })
+
+app.use('/api/user', userRoute)
